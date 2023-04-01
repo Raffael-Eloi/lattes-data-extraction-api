ec11181299af2942a5e05e1fc67e53591bfd7fbf
Raffael Eloi <raffaeleloi121@gmail.com>
2023-04-01 10:50:21 -0300

    AcademicResearcherFileReadServiceShould Get_General_Data_Information_From_File(RED)

--- LattesDataExtraction.Domain.Tests/Services/AcademicResearcherFileReadServiceShould.cs
+++ LattesDataExtraction.Domain.Tests/Services/AcademicResearcherFileReadServiceShould.cs
@@ -6,14 +6,20 @@ namespace LattesDataExtraction.Domain.Tests.Services
 {
     public class AcademicResearcherFileReadServiceShould
     {
+        private IAcademicResearcherFileReadService academicResearcherFileReadService;
+
+        [SetUp]
+        public void Setup() 
+        {
+            academicResearcherFileReadService = new AcademicResearcherFileReadService();
+        }
+
         [Test]
-        public void Get_Curriculo_Vitae_Information_From_File()
+        public void Get_Curriculum_Vitae_Information_From_File()
         {
             #region Arrange
 
             var academicResearcherFile = @"C:\useful\researcher.xml";
-
-            IAcademicResearcherFileReadService academicResearcherFileReadService = new AcademicResearcherFileReadService();
 
             #endregion
 
@@ -25,12 +31,58 @@ namespace LattesDataExtraction.Domain.Tests.Services
 
             #region Assert
 
-            string identifierNumber = "7309417394410594";
+            string expectedIdentifierNumber = "7309417394410594";
 
             Assert.That(academicResearcher, Is.Not.Null);
-            Assert.That(academicResearcher.IdentifierNumber, Is.EqualTo(identifierNumber));
+            Assert.That(academicResearcher.IdentifierNumber, Is.EqualTo(expectedIdentifierNumber));
 
             #endregion
+        }
+
+        [Test]
+        public void Get_General_Data_Information_From_File()
+        {
+            #region Arrange
+
+            var academicResearcherFile = @"C:\useful\researcher.xml";
+
+            #endregion
+
+            #region Act
+
+            AcademicResearch? academicResearcher = academicResearcherFileReadService.GetAcademicInformation(academicResearcherFile);
+
+            #endregion
+
+            #region Assert
+
+            string expectedFullName = "Fabiano Fagundes";
+            string expectedCitationName = "FAGUNDES, Fabiano;FAGUNDES, FABIANO";
+            string expectedNationality = "B";
+            string expectedCountryOfBirth = "Brasil";
+            string expectedStateOfBirth = "SC";
+            string expectedCityOfBirth = "Florianópolis";
+            string expectedCountryCode = "BRA";;
+            string expectedNationalityCountry = "Brasil";
+            string expectedOrcidId = "https://orcid.org/0000-0001-9006-6238";
+
+            Assert.That(academicResearcher, Is.Not.Null);
+            
+            Assert.Multiple(() =>
+            {
+                Assert.That(academicResearcher.FullName, Is.EqualTo(expectedFullName));
+                Assert.That(academicResearcher.CitationName, Is.EqualTo(expectedCitationName));
+                Assert.That(academicResearcher.Nationality, Is.EqualTo(expectedNationality));
+                Assert.That(academicResearcher.CountryOfBirth, Is.EqualTo(expectedCountryOfBirth));
+                Assert.That(academicResearcher.StateOfBirth, Is.EqualTo(expectedStateOfBirth));
+                Assert.That(academicResearcher.CityOfBirth, Is.EqualTo(expectedCityOfBirth));
+                Assert.That(academicResearcher.CountryCode, Is.EqualTo(expectedCountryCode));
+                Assert.That(academicResearcher.NationalityCountry, Is.EqualTo(expectedNationalityCountry));
+                Assert.That(academicResearcher.OrcidId, Is.EqualTo(expectedOrcidId));
+            });
+
+            #endregion
+
         }
     }
 }
--- LattesDataExtraction.Domain/Entities/AcademicResearch.cs
+++ LattesDataExtraction.Domain/Entities/AcademicResearch.cs
@@ -3,5 +3,23 @@
     public class AcademicResearch
     {
         public string IdentifierNumber { get; set; } = string.Empty;
+
+        public string FullName { get; set; } = string.Empty;
+
+        public string CitationName { get; set; } = string.Empty;
+
+        public string Nationality { get; set; } = string.Empty;
+
+        public string CountryOfBirth { get; set; } = string.Empty;
+
+        public string StateOfBirth { get; set; } = string.Empty;
+
+        public string CityOfBirth { get; set; } = string.Empty;
+
+        public string CountryCode { get; set; } = string.Empty;
+
+        public string NationalityCountry { get; set; } = string.Empty;
+
+        public string OrcidId { get; set; } = string.Empty;
     }
 }
