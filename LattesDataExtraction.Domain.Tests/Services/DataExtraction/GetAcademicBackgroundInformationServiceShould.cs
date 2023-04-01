6e0b3149bddc00cd8768dccf15bf8871bfe8b727
Raffael Eloi <raffaeleloi121@gmail.com>
2023-04-01 18:44:31 -0300

    GetAcademicBackgroundInformationServiceShould  Get_Specialization_Information_From_Xml_Document (RED)

--- LattesDataExtraction.Domain.Tests/Services/DataExtraction/GetAcademicBackgroundInformationServiceShould.cs
+++ LattesDataExtraction.Domain.Tests/Services/DataExtraction/GetAcademicBackgroundInformationServiceShould.cs
@@ -63,5 +63,46 @@ namespace LattesDataExtraction.Domain.Tests.Services.DataExtraction
 
             #endregion
         }
+
+        [Test]
+        public void Get_Specialization_Information_From_Xml_Document()
+        {
+            #region Arrange
+
+            academicResearcherDocument.Load(academicResearcherFilePath);
+
+            AcademicResearcher academicResearcher = new();
+
+            #endregion
+
+            #region Act
+
+            getAcademicBackgroundInformationService.GetInformation(academicResearcher, academicResearcherDocument);
+
+            #endregion
+
+            #region Assert
+
+            string espectedTechCourseCode = "90000016";
+            string espectedTechCourseName = "Tecnologias Digitais Aplicadas à Educação";
+
+            string espectedPsychologyCourseCode = "90000014";
+            string espectedPsychologyCourseName = "Psicologia positiva, ciência do bem-estar e autorrealização";
+
+
+            var techCourseVerification = academicResearcher.AcademicBackgrounds.FirstOrDefault(x => x.CourseCode == espectedTechCourseCode && x.CourseName == espectedTechCourseName);
+
+            var psychologyVerification = academicResearcher.AcademicBackgrounds.FirstOrDefault(x => x.CourseCode == espectedPsychologyCourseCode && x.CourseName == espectedPsychologyCourseName);
+
+            Assert.That(academicResearcher, Is.Not.Null);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(techCourseVerification, Is.Not.Null);
+                Assert.That(psychologyVerification, Is.Not.Null);
+            });
+
+            #endregion
+        }
     }
 }
