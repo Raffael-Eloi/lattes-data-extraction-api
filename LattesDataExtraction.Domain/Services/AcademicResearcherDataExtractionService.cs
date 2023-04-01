ed977700201cabb09db5f108888efac7dd5108c3
Raffael Eloi <raffaeleloi121@gmail.com>
2023-04-01 15:18:50 -0300

    GetGeneralDataInformationServiceShould Get_Information_From_Xml_Document (GREEN)

--- LattesDataExtraction.Domain/Services/AcademicResearcherDataExtractionService.cs
+++ LattesDataExtraction.Domain/Services/AcademicResearcherDataExtractionService.cs
@@ -127,7 +127,6 @@ namespace LattesDataExtraction.Domain.Services
                         _academicResearch!.OrcidId = orcidId.Value;
                     }
                 }
-
             }
         }
         
---
+++ LattesDataExtraction.Domain/Services/DataExtraction/GetGeneralDataInformationService.cs
@@ -0,0 +1,85 @@
+﻿using LattesDataExtraction.Domain.Contracts;
+using LattesDataExtraction.Domain.Entities;
+using System.Xml;
+
+namespace LattesDataExtraction.Domain.Services.DataExtraction
+{
+    internal class GetGeneralDataInformationService : IGetDataInformationService
+    {
+        public void GetInformation(AcademicResearcher academicResearcher, XmlDocument academicResearcherDocument)
+        {
+            XmlNodeList generalDataInformation = academicResearcherDocument.GetElementsByTagName("DADOS-GERAIS");
+
+            if (generalDataInformation.Count < 0) return;
+
+            foreach (XmlNode element in generalDataInformation)
+            {
+                if (element.Attributes is not null && element.Attributes.Count > 0)
+                {
+                    XmlAttribute? fullName = element.Attributes["NOME-COMPLETO"];
+
+                    if (fullName is not null)
+                    {
+                        academicResearcher.FullName = fullName.Value;
+                    }
+
+                    XmlAttribute? citationName = element.Attributes["NOME-EM-CITACOES-BIBLIOGRAFICAS"];
+
+                    if (citationName is not null)
+                    {
+                        academicResearcher.CitationName = citationName.Value;
+                    }
+
+                    XmlAttribute? nationality = element.Attributes["NACIONALIDADE"];
+
+                    if (nationality is not null)
+                    {
+                        academicResearcher.Nationality = nationality.Value;
+                    }
+
+                    XmlAttribute? countryOfBirth = element.Attributes["PAIS-DE-NASCIMENTO"];
+
+                    if (countryOfBirth is not null)
+                    {
+                        academicResearcher.CountryOfBirth = countryOfBirth.Value;
+                    }
+
+                    XmlAttribute? stateOfBirth = element.Attributes["UF-NASCIMENTO"];
+
+                    if (stateOfBirth is not null)
+                    {
+                        academicResearcher.StateOfBirth = stateOfBirth.Value;
+                    }
+
+                    XmlAttribute? cityOfBirth = element.Attributes["CIDADE-NASCIMENTO"];
+
+                    if (cityOfBirth is not null)
+                    {
+                        academicResearcher.CityOfBirth = cityOfBirth.Value;
+                    }
+
+                    XmlAttribute? countryCode = element.Attributes["SIGLA-PAIS-NACIONALIDADE"];
+
+                    if (countryCode is not null)
+                    {
+                        academicResearcher.CountryCode = countryCode.Value;
+                    }
+
+                    XmlAttribute? nationalityCountry = element.Attributes["PAIS-DE-NACIONALIDADE"];
+
+                    if (nationalityCountry is not null)
+                    {
+                        academicResearcher.NationalityCountry = nationalityCountry.Value;
+                    }
+
+                    XmlAttribute? orcidId = element.Attributes["ORCID-ID"];
+
+                    if (orcidId is not null)
+                    {
+                        academicResearcher.OrcidId = orcidId.Value;
+                    }
+                }
+            }
+        }
+    }
+}
