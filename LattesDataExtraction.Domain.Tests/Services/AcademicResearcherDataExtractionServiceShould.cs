fce3493b9928e15ba31174f4b67d4d7cd7a005e8
Raffael Eloi <raffaeleloi121@gmail.com>
2023-04-01 16:12:23 -0300

    AcademicResearcherDataExtractionServiceShould Extract_Information_From_File (REFACTOR)

--- LattesDataExtraction.Domain.Tests/Services/AcademicResearcherDataExtractionServiceShould.cs
+++ LattesDataExtraction.Domain.Tests/Services/AcademicResearcherDataExtractionServiceShould.cs
@@ -1,5 +1,6 @@
 using LattesDataExtraction.Domain.Contracts;
 using LattesDataExtraction.Domain.Entities;
+using LattesDataExtraction.Domain.Factories;
 using LattesDataExtraction.Domain.Services;
 
 namespace LattesDataExtraction.Domain.Tests.Services
@@ -8,14 +9,18 @@ namespace LattesDataExtraction.Domain.Tests.Services
     {
         private IAcademicResearcherDataExtractionService academicResearcherFileReadService;
 
+        private IGetDataInformationFactory getDataInformationFactory;
+
         [SetUp]
         public void Setup() 
         {
-            academicResearcherFileReadService = new AcademicResearcherDataExtractionService();
+            getDataInformationFactory = new GetDataInformationFactory();
+
+            academicResearcherFileReadService = new AcademicResearcherDataExtractionService(getDataInformationFactory);
         }
 
         [Test]
-        public void Get_Curriculum_Vitae_Information_From_File()
+        public void Extract_Information_From_File()
         {
             #region Arrange
 
@@ -31,137 +36,7 @@ namespace LattesDataExtraction.Domain.Tests.Services
 
             #region Assert
 
-            string expectedIdentifierNumber = "7309417394410594";
-
             Assert.That(academicResearcher, Is.Not.Null);
-            Assert.That(academicResearcher.IdentifierNumber, Is.EqualTo(expectedIdentifierNumber));
-
-            #endregion
-        }
-
-        [Test]
-        public void Get_General_Data_Information_From_File()
-        {
-            #region Arrange
-
-            var academicResearcherFile = @"C:\useful\researcher.xml";
-
-            #endregion
-
-            #region Act
-
-            AcademicResearcher? academicResearcher = academicResearcherFileReadService.GetAcademicInformation(academicResearcherFile);
-
-            #endregion
-
-            #region Assert
-
-            string expectedFullName = "Fabiano Fagundes";
-            string expectedCitationName = "FAGUNDES, Fabiano;FAGUNDES, FABIANO";
-            string expectedNationality = "B";
-            string expectedCountryOfBirth = "Brasil";
-            string expectedStateOfBirth = "SC";
-            string expectedCityOfBirth = "Florianópolis";
-            string expectedCountryCode = "BRA";;
-            string expectedNationalityCountry = "Brasil";
-            string expectedOrcidId = "https://orcid.org/0000-0001-9006-6238";
-
-            Assert.That(academicResearcher, Is.Not.Null);
-            
-            Assert.Multiple(() =>
-            {
-                Assert.That(academicResearcher.FullName, Is.EqualTo(expectedFullName));
-                Assert.That(academicResearcher.CitationName, Is.EqualTo(expectedCitationName));
-                Assert.That(academicResearcher.Nationality, Is.EqualTo(expectedNationality));
-                Assert.That(academicResearcher.CountryOfBirth, Is.EqualTo(expectedCountryOfBirth));
-                Assert.That(academicResearcher.StateOfBirth, Is.EqualTo(expectedStateOfBirth));
-                Assert.That(academicResearcher.CityOfBirth, Is.EqualTo(expectedCityOfBirth));
-                Assert.That(academicResearcher.CountryCode, Is.EqualTo(expectedCountryCode));
-                Assert.That(academicResearcher.NationalityCountry, Is.EqualTo(expectedNationalityCountry));
-                Assert.That(academicResearcher.OrcidId, Is.EqualTo(expectedOrcidId));
-            });
-
-            #endregion
-        }
-        
-        [Test]
-        public void Get_Professional_Description_Information_From_File()
-        {
-            #region Arrange
-
-            var academicResearcherFile = @"C:\useful\researcher.xml";
-
-            #endregion
-
-            #region Act
-
-            AcademicResearcher? academicResearcher = academicResearcherFileReadService.GetAcademicInformation(academicResearcherFile);
-
-            #endregion
-
-            #region Assert
-
-            string expectedProfessionalDescription = @"Bacharel em Ciências da Computação pela Universidade Federal de Santa Catarina - UFSC (1995) e em Psicologia pelo Centro Universitário Luterano de Palmas - CEULP/ULBRA (2012) , com especialização em Redes de Computadores e Inteligência Artificial (2002) e mestrado em Ciências da Computação também pela Universidade Federal de Santa Catarina (2002). Está cursando doutorado em Ensino de Ciências e Matemática pelo PPGECIM-ULBRA. Atualmente é Professor do Centro Universitário Luterano de Palmas nos Cursos de Bacharelado em Sistemas de Informação, Ciência da Computação e Engenharia de Software e também Coordenador Adjunto do curso de Engenharia de Software. Participa dos Conselhos dos Cursos de Sistemas de Informação, Ciência da Computação e Engenharia de Software. É membro ativo do Conselho de Ensino, Pesquisa e extensão (CONSEPE) e do Conselho Superior (CONSUP), tendo participado do Comitê Científico e coordenado a Comissão Própria de Avaliação do CEULP/ULBRA. Tem pesquisas relacionadas à Tecnologia e Qualidade de Vida bem como em Novas Tecnologias Aplicadas na Educação.";
-
-            Assert.That(academicResearcher, Is.Not.Null);
-            
-            Assert.Multiple(() =>
-            {
-                Assert.That(academicResearcher.ProfessionalDescription, Is.EqualTo(expectedProfessionalDescription));
-            });
-
-            #endregion
-        }
-
-        [Test]
-        public void Get_Professional_Address_Information_From_File()
-        {
-            #region Arrange
-
-            var academicResearcherFile = @"C:\useful\researcher.xml";
-
-            #endregion
-
-            #region Act
-
-            AcademicResearcher? academicResearcher = academicResearcherFileReadService.GetAcademicInformation(academicResearcherFile);
-
-            #endregion
-
-            #region Assert
-
-            string expectedCompanyCode = "000100000991";
-            string expectedCompanyName = "Centro Universitário Luterano de Palmas";
-            string expectedAddressComplement = "Av. Teotônio Segurado s/n";
-            string expectedCountry = "Brasil";
-            string expectedState = "TO";
-            string expectedZipCode = "77054-970";
-            string expectedCity = "Palmas";
-            string expectedDistinct = "1501 SUL";
-            string expectedDDD = "63";
-            string expectedPhoneNumber= "32198081";
-            string expectedFax= "32198000";
-            string expectedPOBox= "160";
-            string expectedHomePage = "http://www.ulbra-to.br";
-
-            Assert.That(academicResearcher, Is.Not.Null);
-
-            Assert.Multiple(() =>
-            {
-                Assert.That(academicResearcher.ProfessionalAddress.CompanyCode, Is.EqualTo(expectedCompanyCode));
-                Assert.That(academicResearcher.ProfessionalAddress.CompanyName, Is.EqualTo(expectedCompanyName));
-                Assert.That(academicResearcher.ProfessionalAddress.AddressComplement, Is.EqualTo(expectedAddressComplement));
-                Assert.That(academicResearcher.ProfessionalAddress.Country, Is.EqualTo(expectedCountry));
-                Assert.That(academicResearcher.ProfessionalAddress.State, Is.EqualTo(expectedState));
-                Assert.That(academicResearcher.ProfessionalAddress.ZipCode, Is.EqualTo(expectedZipCode));
-                Assert.That(academicResearcher.ProfessionalAddress.City, Is.EqualTo(expectedCity));
-                Assert.That(academicResearcher.ProfessionalAddress.District, Is.EqualTo(expectedDistinct));
-                Assert.That(academicResearcher.ProfessionalAddress.DDD, Is.EqualTo(expectedDDD));
-                Assert.That(academicResearcher.ProfessionalAddress.PhoneNumber, Is.EqualTo(expectedPhoneNumber));
-                Assert.That(academicResearcher.ProfessionalAddress.Fax, Is.EqualTo(expectedFax));
-                Assert.That(academicResearcher.ProfessionalAddress.POBox, Is.EqualTo(expectedPOBox));
-                Assert.That(academicResearcher.ProfessionalAddress.HomePage, Is.EqualTo(expectedHomePage));
-            });
 
             #endregion
         }
--- LattesDataExtraction.Domain/Services/AcademicResearcherDataExtractionService.cs
+++ LattesDataExtraction.Domain/Services/AcademicResearcherDataExtractionService.cs
@@ -1,22 +1,21 @@
 ﻿using LattesDataExtraction.Domain.Contracts;
 using LattesDataExtraction.Domain.Entities;
+using LattesDataExtraction.Domain.Enums;
 using System.Xml;
 
 namespace LattesDataExtraction.Domain.Services
 {
     internal class AcademicResearcherDataExtractionService : IAcademicResearcherDataExtractionService
     {
-        private readonly IGetDataInformationService _getCurriculumVitaeInformationService;
-
-        private readonly IGetDataInformationService _getGeneralDataInformationService;
+        private readonly IGetDataInformationFactory _getDataInformationFactory;
 
         private XmlDocument? _academicResearcherDocument;
 
         private AcademicResearcher? _academicResearch;
 
-        public AcademicResearcherDataExtractionService(
-         )
+        public AcademicResearcherDataExtractionService(IGetDataInformationFactory getDataInformationFactory)
         {
+            _getDataInformationFactory = getDataInformationFactory;
         }
 
         public AcademicResearcher? GetAcademicInformation(string academicResearcherFile)
@@ -50,136 +49,30 @@ namespace LattesDataExtraction.Domain.Services
 
         private void GetCurriculumInformationIfExists()
         {
-            _getCurriculumVitaeInformationService.GetInformation(_academicResearch!, _academicResearcherDocument);
+            IGetDataInformationService getDataInformationService = _getDataInformationFactory.Create(DataInformationType.CurriculumVitae);
+
+            getDataInformationService.GetInformation(_academicResearch!, _academicResearcherDocument);
         }
 
         private void GetGeneralDataInformationIfExists()
         {
-            _getGeneralDataInformationService.GetInformation(_academicResearch!, _academicResearcherDocument);
+            IGetDataInformationService getDataInformationService = _getDataInformationFactory.Create(DataInformationType.GeneralData);
+
+            getDataInformationService.GetInformation(_academicResearch!, _academicResearcherDocument);
         }
         
         private void GetProfessionalDescriptionInformationIfExists()
         {
-            XmlNodeList professionalDescriptionInformation = _academicResearcherDocument.GetElementsByTagName("RESUMO-CV");
+            IGetDataInformationService getDataInformationService = _getDataInformationFactory.Create(DataInformationType.ProfessionalDescription);
 
-            if (professionalDescriptionInformation.Count < 0) return;
-
-            foreach (XmlNode element in professionalDescriptionInformation)
-            {
-                if (element.Attributes is not null && element.Attributes.Count > 0)
-                {
-                    XmlAttribute? professionalDescription = element.Attributes["TEXTO-RESUMO-CV-RH"];
-
-                    if (professionalDescription is not null)
-                    {
-                        _academicResearch!.ProfessionalDescription = professionalDescription.Value;
-                    }
-                }
-            }
+            getDataInformationService.GetInformation(_academicResearch!, _academicResearcherDocument);
         }
         
         private void GetProfessionalAddressInformationIfExists()
         {
-            XmlNodeList professionalAddressInformation = _academicResearcherDocument.GetElementsByTagName("ENDERECO-PROFISSIONAL");
+            IGetDataInformationService getDataInformationService = _getDataInformationFactory.Create(DataInformationType.ProfessionalAddress);
 
-            if (professionalAddressInformation.Count < 0) return;
-
-            foreach (XmlNode element in professionalAddressInformation)
-            {
-                if (element.Attributes is not null && element.Attributes.Count > 0)
-                {
-                    XmlAttribute? companyCode = element.Attributes["CODIGO-INSTITUICAO-EMPRESA"];
-
-                    if (companyCode is not null)
-                    {
-                        _academicResearch!.ProfessionalAddress.CompanyCode = companyCode.Value;
-                    }
-
-                    XmlAttribute? companyName = element.Attributes["NOME-INSTITUICAO-EMPRESA"];
-
-                    if (companyName is not null)
-                    {
-                        _academicResearch!.ProfessionalAddress.CompanyName = companyName.Value;
-                    }
-
-                    XmlAttribute? addressComplement = element.Attributes["LOGRADOURO-COMPLEMENTO"];
-
-                    if (addressComplement is not null)
-                    {
-                        _academicResearch!.ProfessionalAddress.AddressComplement = addressComplement.Value;
-                    }
-
-                    XmlAttribute? country = element.Attributes["PAIS"];
-
-                    if (country is not null)
-                    {
-                        _academicResearch!.ProfessionalAddress.Country = country.Value;
-                    }
-
-                    XmlAttribute? state = element.Attributes["UF"];
-
-                    if (state is not null)
-                    {
-                        _academicResearch!.ProfessionalAddress.State = state.Value;
-                    }
-
-                    XmlAttribute? zipCode = element.Attributes["CEP"];
-
-                    if (zipCode is not null)
-                    {
-                        _academicResearch!.ProfessionalAddress.ZipCode = zipCode.Value;
-                    }
-
-                    XmlAttribute? city = element.Attributes["CIDADE"];
-
-                    if (city is not null)
-                    {
-                        _academicResearch!.ProfessionalAddress.City = city.Value;
-                    }
-
-                    XmlAttribute? district = element.Attributes["BAIRRO"];
-
-                    if (district is not null)
-                    {
-                        _academicResearch!.ProfessionalAddress.District = district.Value;
-                    }
-
-                    XmlAttribute? ddd = element.Attributes["DDD"];
-
-                    if (ddd is not null)
-                    {
-                        _academicResearch!.ProfessionalAddress.DDD = ddd.Value;
-                    }
-
-                    XmlAttribute? phoneNumber = element.Attributes["TELEFONE"];
-
-                    if (phoneNumber is not null)
-                    {
-                        _academicResearch!.ProfessionalAddress.PhoneNumber = phoneNumber.Value;
-                    }
-                    
-                    XmlAttribute? fax = element.Attributes["FAX"];
-
-                    if (fax is not null)
-                    {
-                        _academicResearch!.ProfessionalAddress.Fax = fax.Value;
-                    }
-                    
-                    XmlAttribute? poBox = element.Attributes["CAIXA-POSTAL"];
-
-                    if (poBox is not null)
-                    {
-                        _academicResearch!.ProfessionalAddress.POBox = poBox.Value;
-                    }
-
-                    XmlAttribute? homePage = element.Attributes["HOME-PAGE"];
-
-                    if (homePage is not null)
-                    {
-                        _academicResearch!.ProfessionalAddress.HomePage = homePage.Value;
-                    }
-                }
-            }
+            getDataInformationService.GetInformation(_academicResearch!, _academicResearcherDocument);
         }
     }
 }
