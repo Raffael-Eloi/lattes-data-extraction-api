48a3df6a793c1bacf76e30a7c3c5286068d15cdb
Raffael Eloi <raffaeleloi121@gmail.com>
2023-04-01 11:34:56 -0300

    AcademicResearcherFileReadServiceShould Get_Professional_Description_Information_From_File (RED)

--- LattesDataExtraction.Domain.Tests/Services/AcademicResearcherFileReadServiceShould.cs
+++ LattesDataExtraction.Domain.Tests/Services/AcademicResearcherFileReadServiceShould.cs
@@ -82,7 +82,35 @@ namespace LattesDataExtraction.Domain.Tests.Services
             });
 
             #endregion
+        }
+        
+        [Test]
+        public void Get_Professional_Description_Information_From_File()
+        {
+            #region Arrange
 
+            var academicResearcherFile = @"C:\useful\researcher.xml";
+
+            #endregion
+
+            #region Act
+
+            AcademicResearch? academicResearcher = academicResearcherFileReadService.GetAcademicInformation(academicResearcherFile);
+
+            #endregion
+
+            #region Assert
+
+            string expectedProfessionalDescription = @"Bacharel em Ciências da Computação pela Universidade Federal de Santa Catarina - UFSC (1995) e em Psicologia pelo Centro Universitário Luterano de Palmas - CEULP/ULBRA (2012) , com especialização em Redes de Computadores e Inteligência Artificial (2002) e mestrado em Ciências da Computação também pela Universidade Federal de Santa Catarina (2002). Está cursando doutorado em Ensino de Ciências e Matemática pelo PPGECIM-ULBRA. Atualmente é Professor do Centro Universitário Luterano de Palmas nos Cursos de Bacharelado em Sistemas de Informação, Ciência da Computação e Engenharia de Software e também Coordenador Adjunto do curso de Engenharia de Software. Participa dos Conselhos dos Cursos de Sistemas de Informação, Ciência da Computação e Engenharia de Software. É membro ativo do Conselho de Ensino, Pesquisa e extensão (CONSEPE) e do Conselho Superior (CONSUP), tendo participado do Comitê Científico e coordenado a Comissão Própria de Avaliação do CEULP/ULBRA. Tem pesquisas relacionadas à Tecnologia e Qualidade de Vida bem como em Novas Tecnologias Aplicadas na Educação.";
+
+            Assert.That(academicResearcher, Is.Not.Null);
+            
+            Assert.Multiple(() =>
+            {
+                Assert.That(academicResearcher.ProfessionalDescription, Is.EqualTo(expectedProfessionalDescription));
+            });
+
+            #endregion
         }
     }
 }
--- LattesDataExtraction.Domain/Entities/AcademicResearch.cs
+++ LattesDataExtraction.Domain/Entities/AcademicResearch.cs
@@ -6,6 +6,8 @@
 
         public string FullName { get; set; } = string.Empty;
 
+        public string ProfessionalDescription { get; set; } = string.Empty;
+
         public string CitationName { get; set; } = string.Empty;
 
         public string Nationality { get; set; } = string.Empty;
