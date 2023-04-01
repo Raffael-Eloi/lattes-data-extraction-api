585dacc44907595c6aa53d9c2bc1d501a8a3e52e
Raffael Eloi <raffaeleloi121@gmail.com>
2023-04-01 15:23:28 -0300

    GetProfessionalDescriptionInformationServiceShould Get_Information_From_Xml_Document (GREEN)

---
+++ LattesDataExtraction.Domain/Services/DataExtraction/GetProfessionalDescriptionInformationService.cs
@@ -0,0 +1,29 @@
+﻿using LattesDataExtraction.Domain.Contracts;
+using LattesDataExtraction.Domain.Entities;
+using System.Xml;
+
+namespace LattesDataExtraction.Domain.Services.DataExtraction
+{
+    internal class GetProfessionalDescriptionInformationService : IGetDataInformationService
+    {
+        public void GetInformation(AcademicResearcher academicResearcher, XmlDocument academicResearcherDocument)
+        {
+            XmlNodeList professionalDescriptionInformation = academicResearcherDocument.GetElementsByTagName("RESUMO-CV");
+
+            if (professionalDescriptionInformation.Count < 0) return;
+
+            foreach (XmlNode element in professionalDescriptionInformation)
+            {
+                if (element.Attributes is not null && element.Attributes.Count > 0)
+                {
+                    XmlAttribute? professionalDescription = element.Attributes["TEXTO-RESUMO-CV-RH"];
+
+                    if (professionalDescription is not null)
+                    {
+                        academicResearcher.ProfessionalDescription = professionalDescription.Value;
+                    }
+                }
+            }
+        }
+    }
+}
