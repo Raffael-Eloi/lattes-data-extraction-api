95ed630121e23914388f48cfa626289b5c6d3df8
Raffael Eloi <raffaeleloi121@gmail.com>
2023-04-02 12:35:44 -0300

    GetPublishedArticlesInformationServiceShould Get_Information_From_Xml_Document (REFACTOR)

--- LattesDataExtraction.Domain/Services/DataExtraction/GetPublishedArticlesInformationService.cs
+++ LattesDataExtraction.Domain/Services/DataExtraction/GetPublishedArticlesInformationService.cs
@@ -6,9 +6,22 @@ namespace LattesDataExtraction.Domain.Services.DataExtraction
 {
     internal class GetPublishedArticlesInformationService : IGetDataInformationService
     {
+        private List<ScientificArticle>? _scientificArticles;
+
+        private ScientificArticle? _scientificArticle;
+
+        private List<Author>? _authors;
+
+        private Author? _author;
+
         public void GetInformation(AcademicResearcher academicResearcher, XmlDocument academicResearcherDocument)
         {
-            List<ScientificArticle> scientificArticles = new();
+            GetDataFromFile(academicResearcher, academicResearcherDocument);
+        }
+
+        private void GetDataFromFile(AcademicResearcher academicResearcher, XmlDocument academicResearcherDocument)
+        {
+            _scientificArticles = new();
 
             XmlNodeList publishedArticlesInformation = academicResearcherDocument!.GetElementsByTagName("ARTIGO-PUBLICADO");
 
@@ -16,122 +29,137 @@ namespace LattesDataExtraction.Domain.Services.DataExtraction
 
             foreach (XmlNode element in publishedArticlesInformation)
             {
-                ScientificArticle scientificArticle = new();
+                _scientificArticle = new();
 
-                List<Author> authors = new();
+                _authors = new();
 
                 if (element.ChildNodes is null || element.ChildNodes.Count == 0) continue;
 
-                foreach( XmlNode articleElement in element.ChildNodes)
+                foreach (XmlNode articleElement in element.ChildNodes)
                 {
-                    Author author = new();
+                    _author = new();
 
                     if (articleElement is null) continue;
 
-                    if (articleElement.Name is not null && articleElement.Name == "DADOS-BASICOS-DO-ARTIGO")
-                    {
-                        if (articleElement.Attributes is null || articleElement.Attributes.Count == 0) continue;
+                    GetArticleBasicData(articleElement);
 
-                        XmlAttributeCollection articles = articleElement.Attributes;
+                    GetArticleDetail(articleElement);
 
-                        foreach (XmlAttribute article in articles)
-                        {
-                            if (article is null || string.IsNullOrEmpty(article.Name) || string.IsNullOrEmpty(article.Value)) continue;
+                    GetAuthor(articleElement);
 
-                            if (article.Name == "TITULO-DO-ARTIGO")
-                            {
-                                scientificArticle.Title = article.Value;
-                            }
+                    _scientificArticle.Authors = _authors;
 
-                            if (article.Name == "ANO-DO-ARTIGO")
-                            {
-                                scientificArticle.ArticleYear = new DateOnly(int.Parse(article.Value), 1, 1);
-                            }
-
-                            if (article.Name == "IDIOMA")
-                            {
-                                scientificArticle.Language = article.Value;
-                            }
-
-                            if (article.Name == "DOI")
-                            {
-                                scientificArticle.DOI = article.Value;
-                            }
-                        }
-
-                        scientificArticles.Add(scientificArticle);
-                    }
-
-                    if (articleElement.Name is not null && articleElement.Name == "DETALHAMENTO-DO-ARTIGO")
-                    {
-                        if (articleElement.Attributes is null || articleElement.Attributes.Count == 0) continue;
-
-                        XmlAttributeCollection articles = articleElement.Attributes;
-
-                        foreach (XmlAttribute article in articles)
-                        {
-                            if (article is null || string.IsNullOrEmpty(article.Name) || string.IsNullOrEmpty(article.Value)) continue;
-
-                            if (article.Name == "TITULO-DO-PERIODICO-OU-REVISTA")
-                            {
-                                scientificArticle.TitleOfJornalOrMagazine = article.Value;
-                            }
-
-                            if (article.Name == "ISSN")
-                            {
-                                scientificArticle.ISSN = article.Value;
-                            }
-
-                            if (article.Name == "VOLUME")
-                            {
-                                scientificArticle.Volume = article.Value;
-                            }
-
-                            if (article.Name == "SERIE")
-                            {
-                                scientificArticle.Series = article.Value;
-                            }
-
-                            if (article.Name == "PAGINA-INICIAL")
-                            {
-                                scientificArticle.InitialPage = int.Parse(article.Value);
-                            }
-                        }
-                    }
-
-                    if (articleElement.Name is not null && articleElement.Name == "AUTORES") 
-                    {
-                        if (articleElement.Attributes is null || articleElement.Attributes.Count == 0) continue;
-
-                        XmlAttributeCollection articles = articleElement.Attributes;
-
-                        foreach (XmlAttribute article in articles)
-                        {
-                            if (article is null || string.IsNullOrEmpty(article.Name) || string.IsNullOrEmpty(article.Value)) continue;
-
-                            if (article.Name == "NOME-COMPLETO-DO-AUTOR")
-                            {
-                                author.FullName = article.Value;
-                            }
-
-                            if (article.Name == "NOME-PARA-CITACAO")
-                            {
-                                author.CitationName = article.Value;
-                            }
-
-                            if (article.Name == "NRO-ID-CNPQ")
-                            {
-                                author.CNPQId = article.Value;
-                            }
-                        }
-
-                        authors.Add(author);
-                    }
-
-                    scientificArticle.Authors = authors;
+                    _scientificArticles.Add(_scientificArticle!);
                 }
             }
-            academicResearcher.PublishedArticles = scientificArticles;
+
+            academicResearcher.PublishedArticles = _scientificArticles;
+        }
+
+        private void GetArticleBasicData(XmlNode articleElement)
+        {
+            if (articleElement.Name is not null && articleElement.Name == "DADOS-BASICOS-DO-ARTIGO")
+            {
+                if (articleElement.Attributes is null || articleElement.Attributes.Count == 0) return;
+
+                XmlAttributeCollection articles = articleElement.Attributes;
+
+                foreach (XmlAttribute article in articles)
+                {
+                    if (article is null || string.IsNullOrEmpty(article.Name) || string.IsNullOrEmpty(article.Value)) continue;
+
+                    if (article.Name == "TITULO-DO-ARTIGO")
+                    {
+                        _scientificArticle!.Title = article.Value;
+                    }
+
+                    if (article.Name == "ANO-DO-ARTIGO")
+                    {
+                        _scientificArticle!.ArticleYear = new DateOnly(int.Parse(article.Value), 1, 1);
+                    }
+
+                    if (article.Name == "IDIOMA")
+                    {
+                        _scientificArticle!.Language = article.Value;
+                    }
+
+                    if (article.Name == "DOI")
+                    {
+                        _scientificArticle!.DOI = article.Value;
+                    }
+                }
+            }
+        }
+
+        private void GetArticleDetail(XmlNode articleElement)
+        {
+            if (articleElement.Name is not null && articleElement.Name == "DETALHAMENTO-DO-ARTIGO")
+            {
+                if (articleElement.Attributes is null || articleElement.Attributes.Count == 0) return;
+
+                XmlAttributeCollection articles = articleElement.Attributes;
+
+                foreach (XmlAttribute article in articles)
+                {
+                    if (article is null || string.IsNullOrEmpty(article.Name) || string.IsNullOrEmpty(article.Value)) continue;
+
+                    if (article.Name == "TITULO-DO-PERIODICO-OU-REVISTA")
+                    {
+                        _scientificArticle!.TitleOfJornalOrMagazine = article.Value;
+                    }
+
+                    if (article.Name == "ISSN")
+                    {
+                        _scientificArticle!.ISSN = article.Value;
+                    }
+
+                    if (article.Name == "VOLUME")
+                    {
+                        _scientificArticle!.Volume = article.Value;
+                    }
+
+                    if (article.Name == "SERIE")
+                    {
+                        _scientificArticle!.Series = article.Value;
+                    }
+
+                    if (article.Name == "PAGINA-INICIAL")
+                    {
+                        _scientificArticle!.InitialPage = int.Parse(article.Value);
+                    }
+                }
+            }
+        }
+        private void GetAuthor(XmlNode articleElement) 
+        {
+            if (articleElement.Name is not null && articleElement.Name == "AUTORES")
+            {
+                if (articleElement.Attributes is null || articleElement.Attributes.Count == 0) return;
+
+                XmlAttributeCollection articles = articleElement.Attributes;
+
+                foreach (XmlAttribute article in articles)
+                {
+                    if (article is null || string.IsNullOrEmpty(article.Name) || string.IsNullOrEmpty(article.Value)) continue;
+
+                    if (article.Name == "NOME-COMPLETO-DO-AUTOR")
+                    {
+                        _author!.FullName = article.Value;
+                    }
+
+                    if (article.Name == "NOME-PARA-CITACAO")
+                    {
+                        _author!.CitationName = article.Value;
+                    }
+
+                    if (article.Name == "NRO-ID-CNPQ")
+                    {
+                        _author!.CNPQId = article.Value;
+                    }
+                }
+
+                _authors!.Add(_author!);
+            }
         }
     }
 }
