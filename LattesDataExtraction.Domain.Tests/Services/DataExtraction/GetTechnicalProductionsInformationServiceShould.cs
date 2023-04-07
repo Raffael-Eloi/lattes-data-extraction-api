1aea456dd34baf21ea167aaaf01a14fe1973cfd6
Raffael Eloi <raffaeleloi121@gmail.com>
2023-04-07 15:23:28 -0300

    GetTechnicalProductionsInformationServiceShould Get_Information_From_Xml_Document (RED)

---
+++ LattesDataExtraction.Domain.Tests/Services/DataExtraction/GetTechnicalProductionsInformationServiceShould.cs
@@ -0,0 +1,110 @@
+﻿using LattesDataExtraction.Domain.Contracts;
+using LattesDataExtraction.Domain.Entities;
+using System.Xml;
+
+namespace LattesDataExtraction.Domain.Tests.Services.DataExtraction
+{
+    internal class GetTechnicalProductionsInformationServiceShould
+    {
+        private IGetDataInformationService getTechnicalProductionsInformationService;
+
+        private string academicResearcherFilePath;
+
+        private XmlDocument academicResearcherDocument;
+
+        [SetUp]
+        public void SetUp()
+        {
+            academicResearcherFilePath = @"C:\useful\researcher.xml";
+
+            academicResearcherDocument = new XmlDocument();
+
+            getTechnicalProductionsInformationService = new GetTechnicalProductionsInformationService();
+        }
+
+        [Test]
+        public void Get_Information_From_Xml_Document()
+        {
+            #region Arrange
+
+            academicResearcherDocument.Load(academicResearcherFilePath);
+
+            AcademicResearcher academicResearcher = new();
+
+            #endregion
+
+            #region Act
+
+            getTechnicalProductionsInformationService.GetInformation(academicResearcher, academicResearcherDocument);
+
+            #endregion
+
+            #region Assert
+
+            string expectedNatureOfWork = "MULTIMIDIA";
+            string expectedTitle = "Viagens Divertidas";
+            int expectedYear = 2000;
+            string expectedCountry = "Brasil";
+            string expectedLanguage = "Português";
+            string expectedPurpose = "Educação infantil";
+            string expectedPlatform = "Windows";
+            string expectedAvailability = "RESTRITA";
+            string expectedFinancingInstituition = "Centro Universitário Luterano de Palmas";
+
+            Author expectedFirstAuthor = new()
+            {
+                FullName = "Fabiano Fagundes",
+                CitationName = "FAGUNDES, Fabiano;FAGUNDES, FABIANO",
+                CNPQId = "7309417394410594"
+            };
+
+            Author expectedSecondAuthor = new()
+            {
+                FullName = "Aislan Max Gomes",
+                CitationName = "GOMES, A. M.",
+            };
+
+            Assert.That(academicResearcher.TechnicalProductions, Is.Not.Null);
+
+            var technicalProductionVerification = academicResearcher.TechnicalProductions
+                .FirstOrDefault(
+                    x =>
+                        x.NatureOfWork == expectedNatureOfWork &&
+                        x.Title == expectedTitle &&
+                        x.Year.Year == expectedYear &&
+                        x.Country == expectedCountry &&
+                        x.Language == expectedLanguage &&
+                        x.Purpose == expectedPurpose &&
+                        x.Platform == expectedPlatform &&
+                        x.Availability == expectedAvailability &&
+                        x.FinancingInstituition == expectedFinancingInstituition
+                );
+
+            Assert.That(technicalProductionVerification, Is.Not.Null);
+
+            Assert.That(technicalProductionVerification.Authors, Is.Not.Null);
+
+            var isFirstAuthorInListOfAuthors = technicalProductionVerification.Authors
+                .FirstOrDefault(
+                    author =>
+                        author.FullName == expectedFirstAuthor.FullName &&
+                        author.CitationName == expectedFirstAuthor.CitationName
+                 );
+
+            var isSecondAuthorInListOfAuthors = technicalProductionVerification.Authors
+                .FirstOrDefault(
+                    author =>
+                        author.FullName == expectedSecondAuthor.FullName &&
+                        author.CitationName == expectedSecondAuthor.CitationName
+                 );
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(isFirstAuthorInListOfAuthors, Is.Not.Null);
+                Assert.That(isSecondAuthorInListOfAuthors, Is.Not.Null);
+            });
+
+            #endregion
+        }
+    }
+}
--- LattesDataExtraction.Domain/Entities/Software.cs
+++ LattesDataExtraction.Domain/Entities/Software.cs
@@ -2,6 +2,24 @@
 {
     public class Software
     {
-        public IEnumerable<Author> Authors { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public string NatureOfWork { get; set; } = string.Empty;
+
+        public string Country { get; set; } = string.Empty;
+        
+        public string Language { get; set; } = string.Empty;
+        
+        public string Purpose { get; set; } = string.Empty;
+        
+        public string Platform { get; set; } = string.Empty;
+        
+        public string Availability { get; set; } = string.Empty;
+        
+        public string FinancingInstituition { get; set; } = string.Empty;
+
+        public DateOnly Year { get; set; }
+
+        public IEnumerable<Author> Authors { get; set; } = Enumerable.Empty<Author>();
     }
 }
