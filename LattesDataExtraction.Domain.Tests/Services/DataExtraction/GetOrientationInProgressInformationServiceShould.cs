b6c5b579d3802a3d25483e872d6ad26ce312c019
Raffael Eloi <raffaeleloi121@gmail.com>
2023-04-22 10:38:08 -0300

    GetOrientationInProgressInformationServiceShould Get_Information_From_Xml_Document (RED)

---
+++ LattesDataExtraction.Domain.Tests/Services/DataExtraction/GetOrientationInProgressInformationServiceShould.cs
@@ -0,0 +1,78 @@
+﻿using LattesDataExtraction.Domain.Contracts;
+using LattesDataExtraction.Domain.Entities;
+using System.Xml;
+
+namespace LattesDataExtraction.Domain.Tests.Services.DataExtraction
+{
+    internal class GetOrientationInProgressInformationServiceShould
+    {
+        private IGetDataInformationService getOrientationInProgressInformationService;
+
+        private string academicResearcherFilePath;
+
+        private XmlDocument academicResearcherDocument;
+
+        [SetUp]
+        public void SetUp()
+        {
+            academicResearcherFilePath = @"C:\useful\researcher.xml";
+
+            academicResearcherDocument = new XmlDocument();
+
+            getOrientationInProgressInformationService = new GetOrientationInProgressInformationService();
+        }
+
+        [Test]
+        public void Get_Information_From_Xml_Document()
+        {
+            #region Arrange
+
+            academicResearcherDocument.Load(academicResearcherFilePath);
+
+            AcademicResearcher academicResearcher = new();
+
+            #endregion
+
+            #region Act
+
+            getOrientationInProgressInformationService.GetInformation(academicResearcher, academicResearcherDocument);
+
+            #endregion
+
+            #region Assert
+
+            string expectedNatureOfWork = "Iniciação Científica";
+            string expectedTitle = "Desenvolvimento de um Portal a partir da Definição de uma Ontologia para o Domínio &quot;Fauna e Flora&quot; do Parque Estadual do Cantão";
+            int expectedYear = 2007;
+            string expectedCountry = "Brasil";
+            string expectedLanguage = "Português";
+
+            string expectedAcademicOrientedFullName = "Rafael Gomes Amorim";
+            string expectedInstitutionCode = "000100000991";
+            string expectedInstitutionName = "Centro Universitário Luterano de Palmas";
+            string expectedCourseCode = "90000003";
+            string expectedCourseName = "Sistemas de Informação";
+
+            Assert.That(academicResearcher.OrientationInProgress, Is.Not.Null);
+
+            var orientationInProgressVerification = academicResearcher.OrientationInProgress
+                .FirstOrDefault(
+                    x =>
+                        x.NatureOfWork == expectedNatureOfWork &&
+                        x.Title == expectedTitle &&
+                        x.Year.Year == expectedYear &&
+                        x.Country == expectedCountry &&
+                        x.Language == expectedLanguage &&
+                        x.AcademicOrientedFullName == expectedAcademicOrientedFullName &&
+                        x.InstitutionCode == expectedInstitutionCode &&
+                        x.InstitutionName == expectedInstitutionName &&
+                        x.CourseCode == expectedCourseCode &&
+                        x.CourseName == expectedCourseName
+                );
+
+            Assert.That(orientationInProgressVerification, Is.Not.Null);
+
+            #endregion
+        }
+    }
+}
