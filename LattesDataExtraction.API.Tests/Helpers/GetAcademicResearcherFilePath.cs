885889c518ee3a2c62069690a2a78927d7458666
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-16 15:47:53 -0300

    fix integration tests to get XML file direct from current project

---
+++ LattesDataExtraction.API.Tests/Helpers/GetAcademicResearcherFilePath.cs
@@ -0,0 +1,11 @@
+﻿namespace LattesDataExtraction.API.Tests.Helpers;
+
+public static class GetAcademicResearcherFilePath
+{
+    public static string Get()
+    {
+        string currentDirectory = Directory.GetCurrentDirectory();
+        string fullFilePath = Path.Combine(currentDirectory, "Data", "lattes-resume-example.xml");
+        return fullFilePath.Replace("bin\\Debug\\net7.0", "");
+    }
+}
--- LattesDataExtraction.API.Tests/StepDefinitions/AddAcademicResearcherStepDefinitions.cs
+++ LattesDataExtraction.API.Tests/StepDefinitions/AddAcademicResearcherStepDefinitions.cs
@@ -1,125 +1,125 @@
+using LattesDataExtraction.API.Tests.Helpers;
 using System.Net.Http.Headers;
 using System.Text.Json;
 
-namespace LattesDataExtraction.API.Tests.StepDefinitions
+namespace LattesDataExtraction.API.Tests.StepDefinitions;
+
+[Binding]
+public class AddAcademicResearcherStepDefinitions
 {
-    [Binding]
-    public class AddAcademicResearcherStepDefinitions
+    private readonly ScenarioContext _scenarioContext;
+
+    private readonly HttpClient _httpClient;
+
+    public AddAcademicResearcherStepDefinitions(ScenarioContext scenarioContext, HttpClient httpClient)
     {
-        private readonly ScenarioContext _scenarioContext;
+        _scenarioContext = scenarioContext;
+        _httpClient = httpClient;
+    }
 
-        private readonly HttpClient _httpClient;
+    [Given(@"I have a XML File of an Academic Researcher")]
+    public void GivenIHaveAXMLFileOfAnAcademicResearcher()
+    {
+        _scenarioContext["filePath"] = GetAcademicResearcherFilePath.Get();
+    }
 
-        public AddAcademicResearcherStepDefinitions(ScenarioContext scenarioContext, HttpClient httpClient)
+    [When(@"I request to Add a new one")]
+    public async Task WhenIRequestToAddANewOne()
+    {
+        var filePath = _scenarioContext.Get<string>("filePath");
+
+        using (MultipartFormDataContent multipartFormContent = new())
         {
-            _scenarioContext = scenarioContext;
-            _httpClient = httpClient;
-        }
+            StreamContent fileStreamContent = new(File.OpenRead(filePath));
+            fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue("text/xml");
 
-        [Given(@"I have a XML File of an Academic Researcher")]
-        public void GivenIHaveAXMLFileOfAnAcademicResearcher()
-        {
-            _scenarioContext["filePath"] = @"C:\useful\researcher.xml";
-        }
+            multipartFormContent.Add(fileStreamContent, name: "file", fileName: "researcher");
 
-        [When(@"I request to Add a new one")]
-        public async Task WhenIRequestToAddANewOne()
-        {
-            var filePath = _scenarioContext.Get<string>("filePath");
-
-            using (MultipartFormDataContent multipartFormContent = new())
-            {
-                StreamContent fileStreamContent = new(File.OpenRead(filePath));
-                fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue("text/xml");
-
-                multipartFormContent.Add(fileStreamContent, name: "file", fileName: "researcher");
-
-                var httpResponse = await _httpClient.PostAsync("https://localhost:32768/LattesDataExtraction", multipartFormContent);
-
-                httpResponse.IsSuccessStatusCode.Should().BeTrue();
-
-                var response = JsonSerializer.Deserialize<JsonElement>(httpResponse.Content.ReadAsStream());
-
-                response.Should().NotBeNull();
-
-                bool success = response.GetProperty("success").GetBoolean();
-                success.Should().BeTrue();
-
-                Guid id = response.GetProperty("academicResearcherId").GetGuid();
-                id.Should().NotBeEmpty();
-
-                _scenarioContext["academicResearcherId"] = id;
-            };
-        }
-
-        [When(@"I request the information of the Academic Researcher added")]
-        public async Task WhenIRequestTheInformationOfTheAcademicResearcherAdded()
-        {
-            Guid academicResearcherId = _scenarioContext.Get<Guid>("academicResearcherId");
-
-            var httpResponse = await _httpClient.GetAsync($"https://localhost:32768/LattesDataExtraction?academicResearcherId={academicResearcherId}");
+            var httpResponse = await _httpClient.PostAsync("https://localhost:32768/LattesDataExtraction", multipartFormContent);
 
             httpResponse.IsSuccessStatusCode.Should().BeTrue();
 
             var response = JsonSerializer.Deserialize<JsonElement>(httpResponse.Content.ReadAsStream());
 
-            _scenarioContext["response"] = response[0];
-        }
+            response.Should().NotBeNull();
 
+            bool success = response.GetProperty("success").GetBoolean();
+            success.Should().BeTrue();
 
-        [Then(@"I should see the data extracted")]
-        public void ThenIShouldSeeTheDataExtracted()
-        {
-            var response = _scenarioContext.Get<JsonElement>("response");
-
-            VerifyIfAcademicResearcherIsValid(response);
-        }
-
-        private static void VerifyIfAcademicResearcherIsValid(JsonElement response)
-        {
-            Guid id = response.GetProperty("id").GetGuid();
+            Guid id = response.GetProperty("academicResearcherId").GetGuid();
             id.Should().NotBeEmpty();
 
-            string? identifierNumber = response.GetProperty("identifierNumber").GetString();
-            identifierNumber.Should().NotBeNullOrEmpty();
+            _scenarioContext["academicResearcherId"] = id;
+        };
+    }
 
-            string? lattesId = response.GetProperty("lattesId").GetString();
-            lattesId.Should().NotBeNullOrEmpty();
-        }
+    [When(@"I request the information of the Academic Researcher added")]
+    public async Task WhenIRequestTheInformationOfTheAcademicResearcherAdded()
+    {
+        Guid academicResearcherId = _scenarioContext.Get<Guid>("academicResearcherId");
+
+        var httpResponse = await _httpClient.GetAsync($"https://localhost:32768/LattesDataExtraction?academicResearcherId={academicResearcherId}");
+
+        httpResponse.IsSuccessStatusCode.Should().BeTrue();
+
+        var response = JsonSerializer.Deserialize<JsonElement>(httpResponse.Content.ReadAsStream());
+
+        _scenarioContext["response"] = response[0];
+    }
 
 
-        [Given(@"I add three Academic Researchers")]
-        public async Task GivenIAddThreeAcademicResearchers()
+    [Then(@"I should see the data extracted")]
+    public void ThenIShouldSeeTheDataExtracted()
+    {
+        var response = _scenarioContext.Get<JsonElement>("response");
+
+        VerifyIfAcademicResearcherIsValid(response);
+    }
+
+    private static void VerifyIfAcademicResearcherIsValid(JsonElement response)
+    {
+        Guid id = response.GetProperty("id").GetGuid();
+        id.Should().NotBeEmpty();
+
+        string? identifierNumber = response.GetProperty("identifierNumber").GetString();
+        identifierNumber.Should().NotBeNullOrEmpty();
+
+        string? lattesId = response.GetProperty("lattesId").GetString();
+        lattesId.Should().NotBeNullOrEmpty();
+    }
+
+
+    [Given(@"I add three Academic Researchers")]
+    public async Task GivenIAddThreeAcademicResearchers()
+    {
+        GivenIHaveAXMLFileOfAnAcademicResearcher();
+
+        for (int i = 0; i < 3; i++)
         {
-            GivenIHaveAXMLFileOfAnAcademicResearcher();
-
-            for (int i=0; i<3; i++)
-            {
-                await WhenIRequestToAddANewOne();
-            }
+            await WhenIRequestToAddANewOne();
         }
+    }
 
-        [When(@"I request all Academic Researchers")]
-        public async Task WhenIRequestAllAcademicResearchers()
+    [When(@"I request all Academic Researchers")]
+    public async Task WhenIRequestAllAcademicResearchers()
+    {
+        var httpResponse = await _httpClient.GetAsync($"https://localhost:32768/LattesDataExtraction");
+
+        httpResponse.IsSuccessStatusCode.Should().BeTrue();
+
+        var response = JsonSerializer.Deserialize<JsonElement>(httpResponse.Content.ReadAsStream());
+
+        _scenarioContext["getAllResponse"] = response;
+    }
+
+    [Then(@"I should see all Academic Researchers added")]
+    public void ThenIShouldSeeAllAcademicResearchersAdded()
+    {
+        var response = _scenarioContext.Get<JsonElement>("getAllResponse");
+
+        for (int i = 0; i < 3; i++)
         {
-            var httpResponse = await _httpClient.GetAsync($"https://localhost:32768/LattesDataExtraction");
-
-            httpResponse.IsSuccessStatusCode.Should().BeTrue();
-
-            var response = JsonSerializer.Deserialize<JsonElement>(httpResponse.Content.ReadAsStream());
-
-            _scenarioContext["getAllResponse"] = response;
-        }
-
-        [Then(@"I should see all Academic Researchers added")]
-        public void ThenIShouldSeeAllAcademicResearchersAdded()
-        {
-            var response = _scenarioContext.Get<JsonElement>("getAllResponse");
-
-            for (int i = 0; i < 3; i++)
-            {
-                VerifyIfAcademicResearcherIsValid(response[i]);
-            }
+            VerifyIfAcademicResearcherIsValid(response[i]);
         }
     }
 }
